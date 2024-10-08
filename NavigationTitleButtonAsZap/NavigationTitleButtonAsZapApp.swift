71337c54f7d508face511fbb13dcddaa931b4a1c
Juliano Vaz <julianoctvaz@gmail.com>
2024-10-08 11:34:00 -0300

    Initial Commit

---
+++ NavigationTitleButtonAsZap/NavigationTitleButtonAsZapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTitleButtonAsZapApp.swift
+//  NavigationTitleButtonAsZap
+//
+//  Created by Juliano on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTitleButtonAsZapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
