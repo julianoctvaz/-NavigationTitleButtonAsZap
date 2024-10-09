817055b188862c2ee2a1d0c25c73f48e522c7ff5
Juliano Vaz <julianoctvaz@gmail.com>
2024-10-09 09:08:45 -0300

    Melhora legibilidade

--- NavigationTitleButtonAsZap/Estruturas.swift
+++ NavigationTitleButtonAsZap/Estruturas.swift
@@ -13,9 +13,18 @@ struct Conversation: Equatable {
 }
 
 let conversations = [
-    Conversation(username: "Alice", lastMessage: "Aloo, como vai por ai? Vamos tomar uma! Tem tempo que não saimos"),
-    Conversation(username: "Luiz", lastMessage: "Estou sentindo sua falta, você melhorou da doença?"),
-    Conversation(username: "Luan", lastMessage: "Pessoal, o basquete está confirmado hoje mesmo? Queria saber se vou ter que levar minha bola, senão vou sem mesmo")
+    Conversation(
+        username: "Alice",
+        lastMessage: "Aloo, como vai por ai? Vamos tomar uma! Tem tempo que não saimos"
+    ),
+    Conversation(
+        username: "Luiz",
+        lastMessage: "Estou sentindo sua falta, você melhorou da doença?"
+    ),
+    Conversation(
+        username: "Luan",
+        lastMessage: "Pessoal, o basquete está confirmado hoje mesmo? Queria saber se vou ter que levar minha bola, senão vou sem mesmo"
+    )
 ]
 
 struct ConversationDetailView: View {
@@ -43,13 +52,16 @@ struct ConversationDetailView: View {
         .navigationTitle(conversation.username)
         .navigationBarBackButtonHidden()
         .toolbar {
+            
             ToolbarItem(placement: .navigationBarLeading) {
                 Button("Voltar") {
                     presentationMode.wrappedValue.dismiss()
                 }
-            }
-        }
-    }
+            } //toolbarItem
+            
+        } //toolbar
+        
+    } //body
 }
 
 struct AllConversationsView: View {
@@ -71,9 +83,9 @@ struct AllConversationsView: View {
                 Button("Voltar") {
                     presentationMode.wrappedValue.dismiss()
                 }
-            }
-        }
-    }
+            } // ToolbarItem
+        } //toolbar
+    } // body
 }
 
 
@@ -88,22 +100,26 @@ struct ContentView: View {
                 Section(header: ButtonsHeaderView()) {
                     
                     ForEach(conversations.indices, id: \.self) { index in
+                        
                         NavigationLink(destination: ConversationDetailView(conversation: conversations[index])
                         ) {
-                            
                             makeCellItem(of: conversations[index], from: index)
                         }
-                    }
+                        
+                    } // forEach
                     .padding(.vertical, 1)
-                }
+                    
+                } // section
                 .padding(.horizontal, 20)
                 
                 Spacer() //sobe secao
                 
-            }
+            } // scrollView
             .padding(.top, -20) //diminui margem branca
-        }
-    }
+            
+        }// navigationsStack
+        
+    } //body
     
     
     private func makeCellItem(of conversation: Conversation, from index: Int) -> some View {
@@ -141,6 +157,7 @@ struct ButtonsHeaderView: View {
             //aqui mora o problema
         }
         .toolbar {
+            
             ToolbarItem(placement: .primaryAction) {
                 Button(
                     action: {
@@ -150,22 +167,27 @@ struct ButtonsHeaderView: View {
                             Image(systemName: "plus")
                         }
                 ).alert(isPresented: $showAlert) { //alerta do botao
+                    
                     Alert(
                         title: Text("Cria nova conversação"),
                         message: Text("Essa tela ainda esta sendo feita"),
                         dismissButton: .default(Text("OK"))
                     )
-                }
-            }
+                } // alert
+                
+            } // ToolbarItem
+            
             ToolbarItem(placement: .principal) {
                 NavigationLink(destination: AllConversationsView()) {
                     Text("Conversas")
                         .font(.headline)
                         .foregroundColor(.blue)
                 }
-            }
-        }
-    }
+            } // ToolbarItem
+            
+        } // toolbar
+        
+    } //body
 }
 
 #Preview {
